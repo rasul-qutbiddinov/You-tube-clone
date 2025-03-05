24fc38dbfcd761f28504f3f8c6340269defb4b8d
rasul-qutbiddinov <rasulqutbiddinov@gmail.com>
2025-03-05 04:39:57 -0700

    Fix rollup issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve( "./src"),
+      "@": "/src",
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodeResolve(), commonjs()],
     },
   },
 });
